b0ec3637e9911b43e392cb0b45390b6aba5b1f1a
IshniyazovFail <filimen@mail.ru>
2022-06-04 15:15:11 +0300

    correction path

--- src/components/Navbar/Pages.tsx
+++ src/components/Navbar/Pages.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import App from "../../App";
 import {AppCounter2} from "../../AppCounter2";
+
+
+export const PATH = {
+    Counter_1:'/counter_1'
+}
 
  export const Pages = () => {
     return (
         <div>
             <Routes>
+                <Route path={'/'} element={<Navigate to={PATH.Counter_1}/>}/>
+
                 <Route path={"/counter_1"} element={<App/>}/>
                 <Route path={"/counter_2"} element={<AppCounter2/>}/>
             </Routes>
